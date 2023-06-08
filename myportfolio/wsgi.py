40193a53b8b2b65e2a9f9110ba49bd3c1775efb6
HAMA Barhamou <hamabarhamou@gmail.com>
2023-06-08 17:10:22 +0100

    Creation de super user

--- myportfolio/wsgi.py
+++ myportfolio/wsgi.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth.models import User
+from django.core.management import call_command
 # from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myportfolio.settings')
@@ -17,3 +19,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myportfolio.settings')
 application = get_wsgi_application()
 # application=WhiteNoise(application)
 app = application
+
+# Vérifier si le superutilisateur existe déjà
+username = os.environ.get('USERNAME')
+password = os.environ.get('PASSWORD')
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, 'admin@example.com', password)
