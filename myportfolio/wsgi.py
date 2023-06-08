766b679869ee053c7d07adc2e36528dd55a9f200
HAMA Barhamou <hamabarhamou@gmail.com>
2023-06-08 23:14:45 +0100

    deployer

--- myportfolio/wsgi.py
+++ myportfolio/wsgi.py
@@ -8,10 +8,13 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth.models import User
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myportfolio.settings')
+if not User.objects.filter(username='admin').exists():
+    # Créer un superutilisateur avec les informations fournies
+    User.objects.create_superuser('username', 'hamabarhamou@gmail.com', 'passw0rd')
 
 application = get_wsgi_application()
 
